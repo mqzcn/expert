a67e3bc364f31f621aea673400854ab5157f620e
mqzcn <57679249+mqzcn@users.noreply.github.com>
2024-12-13 17:40:44 +0000

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -95,11 +95,13 @@ export default function Navbar() {
               <div className="-mr-2 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
                   <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
+                  {(open: boolean) =>
+                    open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )
+                  }
                 </Disclosure.Button>
               </div>
             </div>
