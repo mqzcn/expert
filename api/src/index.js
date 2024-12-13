4b221b38915b22aa696103cae9d274de248bfa8b
mqzcn <57679249+mqzcn@users.noreply.github.com>
2024-12-13 19:38:25 +0000

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://expert-frontend.netlify.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
