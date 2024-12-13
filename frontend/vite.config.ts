e0bed3ba9850e45c08b9af3417725ebfbe935eab
mqzcn <57679249+mqzcn@users.noreply.github.com>
2024-12-13 18:14:30 +0000

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
-    port: 5173,
+    port: 3000,
   },
 });
