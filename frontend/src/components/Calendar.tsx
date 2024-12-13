a152cfd5a2b713e0835f94a9ebc2902bfbef46d7
mqzcn <57679249+mqzcn@users.noreply.github.com>
2024-12-13 17:38:07 +0000

    Update Calendar.tsx

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import { Calendar as BigCalendar, dateFnsLocalizer } from "react-big-calendar";
+import { Calendar as BigCalendar, Views, dateFnsLocalizer } from 'react-big-calendar';
 import format from "date-fns/format";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
