d857c5b5e0ad8aa2451a730c8cbc7ccc4f355fea
Mazen Magzoub <mazenmagzoub@gmail.com>
2025-02-18 18:57:28 +0000

    changes to proxy.js to fix request

--- frontend/api/proxy.js
+++ frontend/api/proxy.js
@@ -9,10 +9,9 @@ export default async function handler(req, res) {
     const response = await fetch(apiUrl, {
       method: req.method,
       headers: {
-        "Content-Type": "application/json",
         ...req.headers, // Forward headers (includes Authorization)
       },
-      body: req.method === "POST" ? JSON.stringify({ ...req.data }) : null,
+      body: req.method === "POST" ? req.body : null,
     });
     console.log({ response });
     const contentType = response.headers.get("content-type");
