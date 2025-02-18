e7e2a5444ba87ef35bfdc064037057b14f24fe73
Mazen Magzoub <mazenmagzoub@gmail.com>
2025-02-18 18:30:27 +0000

    changes to bring back strigify

--- frontend/api/proxy.js
+++ frontend/api/proxy.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
         ...req.headers, // Forward headers (includes Authorization)
       },
-      body: req.method !== "GET" ? req.body : null,
+      body: req.method !== "GET" ? JSON.stringify(req.body) : null,
     });
 
     const contentType = response.headers.get("content-type");
