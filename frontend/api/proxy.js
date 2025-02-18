4448f75f4ce041c3e621e5c5ca725b7e4080eafd
Mazen Magzoub <mazenmagzoub@gmail.com>
2025-02-18 19:06:35 +0000

    debug

--- frontend/api/proxy.js
+++ frontend/api/proxy.js
@@ -9,16 +9,17 @@ export default async function handler(req, res) {
     const response = await fetch(apiUrl, {
       method: req.method,
       headers: {
+        "Content-Type": "application/json; charset=utf-8",
         ...req.headers, // Forward headers (includes Authorization)
       },
-      body: req.method === "POST" ? req.body : null,
+      body: req.method === "POST" ? JSON.stringify({ ...req.body }) : null,
     });
     console.log({ response });
-    // const contentType = response.headers.get("content-type");
-    // const data =
-    //   contentType && contentType.includes("application/json")
-    //     ? await response.json()
-    //     : await response.text();
+    const contentType = response.headers.get("content-type");
+    const data =
+      contentType && contentType.includes("application/json")
+        ? await response.json()
+        : await response.text();
 
     res.status(response.status).json(data);
   } catch (error) {
