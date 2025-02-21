beb272db71f2edcb59653ba5a95b164d15daf331
Mazen Magzoub <mazenmagzoub@gmail.com>
2025-02-21 12:49:07 +0000

    fix calendar imports

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Calendar as BigCalendar, Event } from "react-big-calendar";
+import {
+  Calendar as BigCalendar,
+  dateFnsLocalizer,
+  Event,
+} from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { DateLocalizer } from "react-big-calendar";
 import { format, parse, startOfWeek, getDay } from "date-fns";
-import { enUS } from "date-fns/locale";
+import enUS from "date-fns/locale/en-US";
 
 interface CalendarProps {
   events: BookingEvent[];
@@ -29,16 +32,16 @@ interface BookingEvent extends Event {
 }
 
 // Set up the DateLocalizer
-const localizer = new DateLocalizer({
-  format: (date, formatString) => format(date, formatString, { locale: enUS }),
-  parse: (dateString, formatString) =>
-    parse(dateString, formatString, new Date(), { locale: enUS }),
-  startOfWeek: () => startOfWeek(new Date(), { locale: enUS }),
-  getDay: (date) => getDay(date),
-  firstOfWeek: () => 0, // Sunday = 0, Monday = 1
-  locales: {
-    "en-US": enUS,
-  },
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
 });
 
 // Calendar component definition
