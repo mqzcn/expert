a1bcf2f0ff50b5ef462f57fc72e864f6343e199a
Mazen Magzoub <mazenmagzoub@gmail.com>
2025-06-10 20:04:48 +0100

    amend cookie banner copy

--- frontend/src/components/CookieBanner.tsx
+++ frontend/src/components/CookieBanner.tsx
@@ -29,8 +29,8 @@ const CookieBanner: React.FC = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 flex items-center justify-between z-50 border-t border-gray-700">
       <p className="text-sm mr-4">
-        We use cookies to enhance your experience. By continuing to visit this
-        site you agree to our use of cookies.{" "}
+        We use cookies to enhance your experience. By continuing to visit and
+        use this site you agree to our use of cookies.{" "}
         <a
           href="/cookies"
           className="text-indigo-400 hover:text-white font-bold"
